0669e015f45638f67b74c0272bbf03ca4e7ff62e
Sarenkab <sarenka.sophie.blunk@hs-furtwangen.de>
2020-06-08 20:04:51 +0200

    aufgabe 06 schaff ich nicht

--- Aufgabe05/script.ts
+++ Aufgabe05/script.ts
@@ -1,79 +1,72 @@
-/*
+
 interface Wohnzimmer {
     img: string;
     artikelname: string;
     spruch: string;
-    preis: string;
+    preis: number;
+    kategorie: number;
 }
 
 //Wohnzimmerartikel
-let hängetopf: Wohnzimmer = {img: "Hängetopf.jpg", artikelname: "Hängetopf", spruch: "Hoch hinauf, lass die Pflanzen fliegen", preis: "9.99€" };
-let hohetöpfe: Wohnzimmer = {img: "hohetöpfe.jfif", artikelname: "Hohe Metalltöpfe", spruch: "Hoch höher- der Topf bringt neue Sichten", preis: "9.99€"};
-let kerzenständer: Wohnzimmer = {img: "Kerzenständer.jfif", artikelname: "Kerzenständer", spruch: "Feuer und Flamme- Lass die Wohnung leuchten", preis: "7.99€"};
-let und: Wohnzimmer = { img: "und.png", artikelname: "Deko &", spruch: "Und und- Lass es Leben", preis: "3.00€"};
-let wanduhr: Wohnzimmer = { img: "Wanduhr.jpg", artikelname: "Wanduhr", spruch: "TikTak- Gönn dir den Sound ", preis: "8.99€"};
-let beistelltisch: Wohnzimmer = { img: "beistelltisch.jfif", artikelname: "Beistelltisch", spruch: "Klein aber Fein- Es trägt auch deinen Wein", preis: "15.00€"}; 
-let aufbewahrungstisch: Wohnzimmer = { img: "Aufbewahrungstisch.webp", artikelname: "Tisch mit Aufbewahrungsfunktion", spruch: "Stauraum macht Sinn", preis: "25.00€"};
-let sofa: Wohnzimmer = {img: "sofafürWohnzimmer.jpg", artikelname: "Sofa", spruch: "gemütlich groß- entspann dich mal wieder so richtig", preis: "259.00€"};
-let gelbersessel: Wohnzimmer = {img: "gelbersessel.jfif", artikelname: "Gelber Sessel", spruch: "Setz dich- es ist vorzüglich", preis: "158.00€"};
-let bookcase: Wohnzimmer = {img: "bookcase.webp", artikelname: "Bookcase", spruch: "Nicht nur ein Schrank- es erzählt Geschichte", preis: "80.00€"};
-let teppich: Wohnzimmer = {img: "teppich.webp", artikelname: "Teppich kariert", spruch: "Fabren und Formen - lass es dir nicht nehmen es gemütlich zu haben", preis: "99.99€"};
-let poster: Wohnzimmer = {img: "postercoco.jpg", artikelname: "CoCo Poster", spruch: "Auch die Wand steht auf Marken", preis: "7.00€"};
-let posterset1: Wohnzimmer = {img: "posterset1.jpg", artikelname: "Poster Set", spruch: "Lass die Wand strahlen", preis: "24.95€"};
+let hängetopf: Wohnzimmer = {img: "Hängetopf.jpg", artikelname: "Hängetopf", spruch: "Hoch hinauf, lass die Pflanzen fliegen", preis: 9.99, kategorie: 0 };
+let hohetöpfe: Wohnzimmer = {img: "hohetöpfe.jfif", artikelname: "Hohe Metalltöpfe", spruch: "Hoch höher- der Topf bringt neue Sichten", preis: 9.99, kategorie: 0};
+let kerzenständer: Wohnzimmer = {img: "Kerzenständer.jfif", artikelname: "Kerzenständer", spruch: "Feuer und Flamme- Lass die Wohnung leuchten", preis: 7.99, kategorie: 0};
+let und: Wohnzimmer = { img: "und.png", artikelname: "Deko &", spruch: "Und und- Lass es Leben", preis: 3.00, kategorie: 0};
+let wanduhr: Wohnzimmer = { img: "Wanduhr.jpg", artikelname: "Wanduhr", spruch: "TikTak- Gönn dir den Sound ", preis: 8.99, kategorie: 0};
+let beistelltisch: Wohnzimmer = { img: "beistelltisch.jfif", artikelname: "Beistelltisch", spruch: "Klein aber Fein- Es trägt auch deinen Wein", preis: 15.00, kategorie: 0}; 
+let aufbewahrungstisch: Wohnzimmer = { img: "Aufbewahrungstisch.webp", artikelname: "Tisch mit Aufbewahrungsfunktion", spruch: "Stauraum macht Sinn", preis: 25.00, kategorie: 1};
+let sofa: Wohnzimmer = {img: "sofafürWohnzimmer.jpg", artikelname: "Sofa", spruch: "gemütlich groß- entspann dich mal wieder so richtig", preis: 259.00, kategorie: 1};
+let gelbersessel: Wohnzimmer = {img: "gelbersessel.jfif", artikelname: "Gelber Sessel", spruch: "Setz dich- es ist vorzüglich", preis: 158.00, kategorie: 1};
+let bookcase: Wohnzimmer = {img: "bookcase.webp", artikelname: "Bookcase", spruch: "Nicht nur ein Schrank- es erzählt Geschichte", preis: 80.00, kategorie: 1};
+let teppich: Wohnzimmer = {img: "teppich.webp", artikelname: "Teppich kariert", spruch: "Fabren und Formen - lass es dir nicht nehmen es gemütlich zu haben", preis: 99.99, kategorie: 1};
+let poster: Wohnzimmer = {img: "postercoco.jpg", artikelname: "CoCo Poster", spruch: "Auch die Wand steht auf Marken", preis: 7.00, kategorie: 1};
+let posterset1: Wohnzimmer = {img: "posterset1.jpg", artikelname: "Poster Set", spruch: "Lass die Wand strahlen", preis: 24.95, kategorie: 1};
 
 let wohnzimmerartikel: Wohnzimmer[] = [hängetopf, hohetöpfe, kerzenständer, und, wanduhr, beistelltisch, aufbewahrungstisch, sofa,gelbersessel, bookcase, teppich, poster, posterset1];
 
 
-interface Schreibtisch {
-    img: string;
-    artikelname: string;
-    spruch: string;
-    preis: string;
-}
-//schreibtischartikel
-let tischbeine: Schreibtisch = {img: "Tischbeine.webp", artikelname: "Tischbeine weiß", spruch: "Praktisch gut- Beine machen den Tisch komplett", preis: "9.99€"};
-let holzhocker: Schreibtisch = {img: "holzhocker.webp", artikelname: "Holzhocker im Landhausstil", spruch: "bequem und schick- gebe deinem Büro einen besonderen Hingucker", preis: "35€"};
-let stuhl: Schreibtisch = {img: "stuhl.jpg", artikelname: "Bürostuhl", spruch: "gemütlich gesund- der Stuhl, der dich auf dem weg zum Erfolg begleitet", preis: "70€"};
-let tischplatte: Schreibtisch = {img: "tischplatte.webp", artikelname: "Tischplatte Holz", spruch: "Holzig schön- Die Platte für die Beine", preis: "40€"};
-let regal: Schreibtisch = {img: "regalweiß.webp", artikelname: "weißes Regal", spruch: "immer schön- ein weißes Regal braucht doch wirklich jeder", preis: "9.99€"};
-let kommode: Schreibtisch = {img: "kommode.webp", artikelname: "Hochglanz Kommode", spruch: "gib der Wohnung einen Kick- glänzt und funkelt", preis: "70€"};
-let posterset: Schreibtisch = {img: "posterset.png", artikelname: "Poster- set", spruch: "belebend schön- gebe der Wand ihren Charakter", preis: "35€"};
-let lampe: Schreibtisch = {img: "Lampeneu.jfif", artikelname: "Tischlampe in Holzoptik", spruch: "leuchte hell- genau das tut Sie für dich", preis: "15€"};
-let blumentopf: Schreibtisch = {img: "blumentopf.jpg", artikelname: "Blumentopf set", spruch: "Schlicht weiß- Bring die Weisheit ins Büro", preis: "10€"};
-let topfset: Schreibtisch = {img: "topfset.jfif", artikelname: "Topf-set", spruch: "schlicht und doch besonders- gib der Pflanze ihr Zuhause", preis: "30€"};
-
-let schreibtischartikel: Schreibtisch[] = [tischbeine, holzhocker, stuhl, tischplatte, regal, kommode, posterset, lampe, blumentopf, topfset];
-
 //wohnzimmerartikel
-for (let index: number = 0; index < wohnzimmerartikel.length; index++) {
+for (let i: number = 0; i < wohnzimmerartikel.length; i++) {
+
+    if (wohnzimmerartikel[i].kategorie == 0) {
+    let newDiv: HTMLDivElement = document.createElement("div");
+    newDiv.id = "div" + i;
+    document.getElementById("wohnartikel")?.appendChild(newDiv); 
+    }
+    if (wohnzimmerartikel[i].kategorie == 1) {
+        let newDiv: HTMLDivElement = document.createElement("div");
+        newDiv.id = "div" + i;
+        document.getElementById("büroartikel")?.appendChild(newDiv);
+    }
     //DIV
     let newDiv: HTMLDivElement = document.createElement("div");
-    newDiv.id = "div1" + index;
+    newDiv.id = "div1" + i;
     document.getElementById("artikel1")?.appendChild(newDiv);
 
     //IMG
     let imgElement: HTMLImageElement = document.createElement("img");
-    imgElement.src = wohnzimmerartikel[index].img;
-    document.getElementById("div1" + index)?.appendChild(imgElement);
+    imgElement.src = wohnzimmerartikel[i].img;
+    document.getElementById("div1" + i)?.appendChild(imgElement);
 
     //artikelname
     let artikelname: HTMLParagraphElement = document.createElement("p");
-    artikelname.innerHTML = wohnzimmerartikel[index]. artikelname;
-    document.getElementById("div1" + index)?.appendChild(artikelname);
+    artikelname.innerHTML = wohnzimmerartikel[i]. artikelname;
+    document.getElementById("div1" + i)?.appendChild(artikelname);
 
     //spruch
     let spruch: HTMLParagraphElement = document.createElement("p");
-    spruch.innerHTML = wohnzimmerartikel[index].spruch;
-    document.getElementById("div1" + index)?.appendChild(spruch);
+    spruch.innerHTML = wohnzimmerartikel[i].spruch;
+    document.getElementById("div1" + i)?.appendChild(spruch);
 
     //PREIS
     let price: HTMLParagraphElement = document.createElement("p");
-    price.innerHTML = wohnzimmerartikel[index].preis;
-    document.getElementById("div1" + index)?.appendChild(price);
+    price.innerHTML = wohnzimmerartikel[i].preis + "€";
+    document.getElementById("div1" + i)?.appendChild(price);
 
     //In den Warenkorb
     let kaufen: HTMLButtonElement = document.createElement("button");
     kaufen.innerHTML = "In den Warenkorb";
-    document.getElementById("div1" + index)?.appendChild(kaufen);
+    document.getElementById("div1" + i)?.appendChild(kaufen);
 }
-*/
+
+
---
+++ Aufgabe06/script.js
@@ -0,0 +1,105 @@
+"use strict";
+var Aufgabe06;
+(function (Aufgabe06) {
+    //dekoartikel
+    let hängetopf = { img: "Hängetopf.jpg", artikelname: "Hängetopf", spruch: "Hoch hinauf, lass die Pflanzen fliegen", preis: 9.99, kategorie: 0 };
+    let hohetöpfe = { img: "hohetöpfe.jfif", artikelname: "Hohe Metalltöpfe", spruch: "Hoch höher- der Topf bringt neue Sichten", preis: 9.99, kategorie: 0 };
+    let kerzenständer = { img: "Kerzenständer.jfif", artikelname: "Kerzenständer", spruch: "Feuer und Flamme- Lass die Wohnung leuchten", preis: 7.99, kategorie: 0 };
+    let und = { img: "und.png", artikelname: "Deko &", spruch: "Und und- Lass es Leben", preis: 3.00, kategorie: 0 };
+    let wanduhr = { img: "Wanduhr.jpg", artikelname: "Wanduhr", spruch: "TikTak- Gönn dir den Sound ", preis: 8.99, kategorie: 0 };
+    let beistelltisch = { img: "beistelltisch.jfif", artikelname: "Beistelltisch", spruch: "Klein aber Fein- Es trägt auch deinen Wein", preis: 15.00, kategorie: 0 };
+    let aufbewahrungstisch = { img: "Aufbewahrungstisch.webp", artikelname: "Tisch mit Aufbewahrungsfunktion", spruch: "Stauraum macht Sinn", preis: 25.00, kategorie: 1 };
+    let sofa = { img: "sofafürWohnzimmer.jpg", artikelname: "Sofa", spruch: "gemütlich groß- entspann dich mal wieder so richtig", preis: 259.00, kategorie: 1 };
+    let gelbersessel = { img: "gelbersessel.jfif", artikelname: "Gelber Sessel", spruch: "Setz dich- es ist vorzüglich", preis: 158.00, kategorie: 1 };
+    let bookcase = { img: "bookcase.webp", artikelname: "Bookcase", spruch: "Nicht nur ein Schrank- es erzählt Geschichte", preis: 80.00, kategorie: 1 };
+    let teppich = { img: "teppich.webp", artikelname: "Teppich kariert", spruch: "Fabren und Formen - lass es dir nicht nehmen es gemütlich zu haben", preis: 99.99, kategorie: 1 };
+    let poster = { img: "postercoco.jpg", artikelname: "CoCo Poster", spruch: "Auch die Wand steht auf Marken", preis: 7.00, kategorie: 1 };
+    let posterset1 = { img: "posterset1.jpg", artikelname: "Poster Set", spruch: "Lass die Wand strahlen", preis: 24.95, kategorie: 1 };
+    let dekoartikel = [hängetopf, hohetöpfe, kerzenständer, und, wanduhr, beistelltisch, aufbewahrungstisch, sofa, gelbersessel, bookcase, teppich, poster, posterset1];
+    for (let index = 0; index < dekoartikel.length; index++) {
+        //DIV
+        if (dekoartikel[index].kategorie == 0) {
+            let newDiv = document.createElement("div");
+            newDiv.id = "div" + index;
+            document.getElementById("artikel0")?.appendChild(newDiv);
+        }
+        if (dekoartikel[index].kategorie == 1) {
+            let newDiv = document.createElement("div");
+            newDiv.id = "div" + index;
+            document.getElementById("artikel1")?.appendChild(newDiv);
+        }
+        //Fotos 
+        let imgElement = document.createElement("img");
+        imgElement.src = dekoartikel[index].img;
+        document.getElementById("div" + index)?.appendChild(imgElement);
+        //artikelname
+        let artikelname = document.createElement("p");
+        artikelname.innerHTML = dekoartikel[index].artikelname;
+        document.getElementById("div" + index)?.appendChild(artikelname);
+        //spruch
+        let spruch = document.createElement("p");
+        spruch.innerHTML = dekoartikel[index].spruch;
+        document.getElementById("div1" + index)?.appendChild(spruch);
+        //Preis
+        let price = document.createElement("p");
+        price.innerHTML = dekoartikel[index].preis + "€";
+        document.getElementById("div1" + index)?.appendChild(price);
+        //In den Warenkorb
+        let kaufen = document.createElement("button");
+        kaufen.innerHTML = "In den Warenkorb";
+        document.getElementById("div" + index)?.appendChild(kaufen);
+        kaufen.addEventListener("klick", handleWarenkorb);
+        kaufen.setAttribute("preis", dekoartikel[index].preis.toString());
+    }
+    //Aufgabe 06
+    let summe = 0;
+    let zahl = 0;
+    let produktzähler = 0;
+    let warenkorbblase = document.createElement("div");
+    function handleWarenkorb(_event) {
+        if (produktzähler >= 0) {
+            document.getElementById("warenkorbzähler")?.appendChild(warenkorbblase);
+        }
+        produktzähler++;
+        warenkorbblase.innerHTML = produktzähler + "";
+        if (_event.currentTarget?.getAttribute("preis")) {
+            summe = zahl + parseInt(_event.currentTarget?.getAttribute("preis"));
+            zahl = summe;
+        }
+        console.log(summe.toFixed(0));
+    }
+    //Aufgabe 06-2
+    let xalles = document.createElement("a");
+    xalles.id = "alleArtikel";
+    xalles.innerHTML = "Alle Dekoartikel";
+    xalles.addEventListener("klick", handleKategorie);
+    document.getElementById("alles")?.appendChild(xalles);
+    let xdekoartikel = document.createElement("a");
+    xdekoartikel.id = "wohnen";
+    xdekoartikel.innerHTML = "Wohnartikel";
+    xdekoartikel.addEventListener("klick", handleKategorie);
+    document.getElementById("Wohnzimmerartikel")?.appendChild(xdekoartikel);
+    let xSchreibtischartikel = document.createElement("a");
+    xSchreibtischartikel.id = "schreib";
+    xSchreibtischartikel.innerHTML = "Büroartikel";
+    xSchreibtischartikel.addEventListener("klick", handleKategorie);
+    document.getElementById("schreibtisch")?.appendChild(xSchreibtischartikel);
+    function handleKategorie(_event) {
+        //Alles  
+        if (_event.currentTarget.getAttribute("id") == "alles") {
+            document.getElementById("artikel0").style.display = "block";
+            document.getElementById("artikel1").style.display = "block";
+            //Wohnzimmer   
+        }
+        else if (_event.currentTarget.getAttribute("id") == "Wohnzimmer") {
+            document.getElementById("artikel0").style.display = "none";
+            document.getElementById("artikel1").style.display = "block";
+            //Schreibtisch   
+        }
+        else if (_event.currentTarget.getAttribute("id") == "schreibtisch") {
+            document.getElementById("artikel0").style.display = "none";
+            document.getElementById("artikel1").style.display = "block";
+        }
+    }
+})(Aufgabe06 || (Aufgabe06 = {}));
+//# sourceMappingURL=script.js.map
