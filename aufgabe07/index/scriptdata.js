b15d30ce09966ac0c6943ce907659d9faa0f0480
Sarenkab <sarenka.sophie.blunk@hs-furtwangen.de>
2020-06-18 13:11:52 +0200

    Fix aufgabe 7

--- aufgabe07/index/scriptdata.js
+++ aufgabe07/index/scriptdata.js
@@ -1,7 +1,8 @@
 "use strict";
 var aufgabe07;
 (function (aufgabe07) {
-    loadWohnzimmer("daten.json");
+    // loadWohnzimmer("http://127.0.0.1:5500/aufgabe07/daten.json");
+    loadWohnzimmer("https://sarenkab.github.io/GiSSommer2020/aufgabe07/daten.json");
     async function loadWohnzimmer(_url) {
         let response = await fetch(_url);
         let jsonArray = await response.json();
