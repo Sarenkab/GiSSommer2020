ca791a4238d88364d5d0b29ff86eb6833ed53f7f
Sarenkab <sarenka.sophie.blunk@hs-furtwangen.de>
2020-06-24 09:37:09 +0200

    neu

--- Aufgabe08Neu/script.ts
+++ Aufgabe08Neu/script.ts
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 namespace A08 {
 
     let formData: FormData;
@@ -25,34 +25,4 @@ namespace A08 {
         console.log(resp2);
     }
 }
-=======
-namespace Aufgabe08 {
 
-    let formData: FormData;
-    let versand: HTMLButtonElement = (<HTMLButtonElement>document.getElementById("button"));
-    versand.addEventListener("click", buttonHandle);
-
-    //Hängt Formulardaten an die URL
-
-    async function addToURL(): Promise<string> {
-        formData = new FormData(document.forms[0]);
-        let url: string = "https://sarenkahasanewapp.herokuapp.com/";
-        let query: URLSearchParams = new URLSearchParams(<any>formData);
-        url = url + "?" + query.toString();
-        return url;
-    }
-
-
-    async function buttonHandle(): Promise<void> {
-        getCommunicate(await addToURL());
-    }
-
-    //Serverantwort
-
-    async function getCommunicate(_url: RequestInfo): Promise<void> {
-        let antwort: Response = await fetch(_url, { method: "get" });
-        let antwort2: string = await antwort.text();
-        console.log(antwort2);
-    }
-}
->>>>>>> master
